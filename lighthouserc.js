2d23cc5dbae2200d384f14310b13217b59f27c9d
ElierEstopinan <elier.estopinan@gmail.com>
2025-10-12 10:45:28 -0400

    lighthouse

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,5 +1,9 @@
 module.exports = {
   ci: {
+    collect: {
+      /* Default local server LHCI should use during CI */
+      serverBaseUrl: 'http://localhost:8080',
+    },
     upload: {
       target: 'temporary-public-storage',
     },
